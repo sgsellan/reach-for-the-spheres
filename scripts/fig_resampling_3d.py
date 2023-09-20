78adc9e0b0e0924ef1de8f1ef66d7486899963a5
Oded Stein <oded.stein.graphics@gmail.com>
2023-09-20 15:49:36 -0700

    resampling 3d

---
+++ scripts/fig_resampling_3d.py
@@ -0,0 +1,32 @@
+# Thanks to our context.py file, we only need this in the header:
+from context import *
+
+# Set up gt
+V_gt, F_gt = gpy.read_mesh('data/cow.obj')
+V_gt = gpy.normalize_points(V_gt)
+
+# Create and abstract SDF function that is the only connection to the shape
+sdf = lambda x: gpy.signed_distance(x, V_gt, F_gt)[0]
+
+
+# Set up a grid and do marching cubes
+n = 20
+gx, gy, gz = np.meshgrid(np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1))
+U = np.vstack((gx.flatten(), gy.flatten(), gz.flatten())).T
+V_mc, F_mc = gpy.marching_cubes(sdf(U), U, n+1, n+1, n+1)
+
+save_dir = 'results/resampling_3d/'
+mesh_exporter_callback = utility.mesh_exporter(save_dir, 1, track_resampling=True)
+def callback(state):
+    # return None
+    mesh_exporter_callback(state)
+V0, F0 = gpy.icosphere(2)
+# working: 0.01, 10
+V,F = gpy.sdf_flow(U, sdf, V0, F0, callback=callback, resample=1,
+    min_h=0.04,
+    verbose=False, visualize=False)
+
+gpy.write_mesh(save_dir+'/initial.obj', V0, F0)
+gpy.write_mesh(save_dir+'/final.obj', V, F)
+gpy.write_mesh(save_dir+'/ground_truth.obj', V_gt, F_gt)
+gpy.write_mesh(save_dir+'/marching_cubes.obj', V_mc, F_mc)
